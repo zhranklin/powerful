5f09b3516479c92e0e0ec8be947ea0f5920348e9
Zhranklin <chigou79@outlook.com>
2021-10-14 03:54:29 +0800

    fix: 非dubbo模式启动

--- powerful-springboot-2/src/main/java/zhranklin/powerful/app/PowerfulApplication.java
+++ powerful-springboot-2/src/main/java/zhranklin/powerful/app/PowerfulApplication.java
@@ -9,7 +9,9 @@ public class PowerfulApplication {
 
     public static void main(String[] args) throws InterruptedException {
         Gen.isStage0 = args.length > 0 && args[0].equals("stage0");
-        Gen.gen(System.getenv("APP"), System.getenv("DUBBO_DEPENDS_ON"));
+        if ("true".equals(System.getProperty("powerful.dubbo.enabled"))) {
+            Gen.gen(System.getenv("APP"), System.getenv("DUBBO_DEPENDS_ON"));
+        }
         if (!Gen.isStage0) {
             SpringApplication.run(PowerfulApplication.class, args);
         }
