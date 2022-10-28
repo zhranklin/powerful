d3034c6a065d7bf1cd78afdd518ac661bac7a65f
lizhenqian01 <lizhenqian01@corp.netease.com>
2022-10-28 16:51:21 +0800

    add webflux demo

--- webflux-view/src/main/java/com/netease/cloud/nsf/webflux/view/WebfluxViewApplication.java
+++ webflux-view/src/main/java/com/netease/cloud/nsf/webflux/view/WebfluxViewApplication.java
@@ -2,14 +2,12 @@ package com.netease.cloud.nsf.webflux.view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-@EnableFeignClients
 public class WebfluxViewApplication
 {
     public static void main( String[] args )
