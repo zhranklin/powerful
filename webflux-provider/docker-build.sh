4703166ac466c4b768ab5dd5549237e0b3cdc817
lizhenqian01 <lizhenqian01@corp.netease.com>
2023-03-02 09:45:23 +0800

    fix webflux demo

---
+++ webflux-provider/docker-build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t harbor.cloud.netease.com/qztest/webflux:provider-26 --platform=linux/amd64 . --push
---
+++ webflux-view/docker-build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t harbor.cloud.netease.com/qztest/webflux:viewer-26 --platform=linux/amd64 . --push
