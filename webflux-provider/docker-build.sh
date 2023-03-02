2af101c9e1212a573eae659542940e1cc21f2bf6
lizhenqian01 <lizhenqian01@corp.netease.com>
2023-03-02 15:59:27 +0800

    fix webflux demo

---
+++ webflux-provider/docker-build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t hub.c.163.com/qztest/webflux:provider-27 --platform=linux/amd64 . --push
---
+++ webflux-view/docker-build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t hub.c.163.com/qztest/webflux:viewer-27 --platform=linux/amd64 . --push
